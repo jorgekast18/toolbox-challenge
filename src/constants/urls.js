acfd1de05dc21ac5d76d6eac7aa5a6a6dbaad3b2
Jorge Castaño <jacr011893@gmail.com>
2025-10-07 08:52:34 -0500

    Modify url base for multiples endpoints

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,4 @@
-export const loginUrl = 'https://echo-serv.tbxnet.com/v1/mobile/auth';
+const urlBase = 'https://echo-serv.tbxnet.com/'
+
+export const loginUrl = `${urlBase}v1/mobile/auth`;
+export const carouselUrl = `${urlBase}v1/mobile/data`
