0b33268d1a41e4e950d70b2dd2ee14d2e5b86e8d
Jorge Castaño <jacr011893@gmail.com>
2025-10-07 09:50:22 -0500

    Add headers for request

--- src/api/carousels.js
+++ src/api/carousels.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 import { loadAbort } from '../utils/load-abort.utility';
 import { carouselUrl } from '../constants/urls';
+import { getHeaders } from './getHeaders';
 
-export const getCarousel = () => {
+
+export const getCarousel = (token) => {
   const controller = loadAbort();
 
   return {
-    call: axios.get(carouselUrl, { signal: controller.signal }),
+    call: axios.get(carouselUrl, {
+      signal: controller.signal,
+      headers: getHeaders(token)
+    }),
     controller
   }
 }
